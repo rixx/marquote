4174611d4d86f9311cf76eae539a4cf1c9237c2e
rixx <git@foyfoy.de>
2014-07-04 20:33:12 +0200

    wrote ProgressBar class and put it in Parser.base

--- marquote/Parser/base.py
+++ marquote/Parser/base.py
@@ -14,3 +14,23 @@ class Sentence():
     def __init__(self, text, char):
         self.text = text
         self.char = char
+
+
+class ProgressBar():
+    def __init__(self, length, name="Progress", width=50):
+        self.bar = "[" + " "*width + "] 0%"
+        self.name = name
+        self.print_name = name + ":" + " "*8
+        self.length = length
+        self.width = width
+
+    def update(self, new_length):
+        percent = int(100 * new_length / self.length)
+        marked = int(percent * self.width / 100)
+        self.bar = self.print_name
+        self.bar += "[" + "=" * marked + " " * (self.width - marked) + "] "
+        self.bar += str(percent) + "%"
+
+        print("\r" + self.bar, end="")
+
+
